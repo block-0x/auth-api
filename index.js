1b66ac7286466e920e27269939416e7f2a99067c
iida <iida@cryptogarage.co.jp>
2022-10-25 17:52:50 +0900

    add api

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require("express");
-
+var crypto = require('crypto')
 const app = express();
+const secret = 'secret'
+const string = 'string'
 
 app.get("/", (req, res) => {
   res.send("Express on Vercel");
+});
+
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+app.get("/signMessage", (req, res) => {
+  // XFJTU1WS3Hmd0r8lZEXEW+MNqDVjqBn9hL3+xY7t/Dk=
+  res.send(crypto.createHmac('SHA256', secret).update(string).digest('base64'));
+});
+
+app.post("/signature", (req, res) => {
+  signature
+});
+
+app.get("/verify", (req, res) => {
+
+  const data = JSON.parse(req);
+  res.send(data);
 });
 
 app.listen(8000, () => {
